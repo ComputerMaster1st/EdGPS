f10d72949800d4ed3bf1bdd6784ace4fbf3f794a
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 13:47:45 +0100

    Now loading instance of starsystem

--- Gps.cs
+++ Gps.cs
@@ -6,9 +6,11 @@ namespace EdGps
     public class Gps
     {
         private JournalReader _reader;
+        private StarSystem _system;
 
         public Gps(string directoryPath) {
             _reader = new JournalReader(directoryPath);
+            _system = StarSystem.Load();
         }
 
         public void Start() {
