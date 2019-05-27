0e2317bc48bc335f6863d2720d83e0a59a04f2e9
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 01:17:48 +0100

    Added directory checking

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using EdGps.Core;
 
 namespace EdGps
@@ -6,8 +7,21 @@ namespace EdGps
     public class Program
     {
         public static void Main(string[] args) {
-            Console.Write("E:D Journal Log Directory :");
-            var directoryPath = Parser.ParseDirectoryPath(Console.ReadLine());
+            Console.Write("Please specify the directory where E:D Journal logs are kept. You may drag & drop if possible");
+            var directoryPath = "";
+            var input = "";
+
+            while (true) {
+                Console.Write("Path:");
+                input = Console.ReadLine();
+
+                if (input.Length < 1) continue;
+
+                directoryPath = Parser.ParseDirectoryPath(input);
+                if (Directory.Exists(directoryPath)) break;
+                
+                Console.WriteLine("Unable to find directory! Please try again.");
+            }
         }
     }
 }
