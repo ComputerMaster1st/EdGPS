c3c619ff0e33e16ed6affb457bbd68b8c47e83d5
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 19:26:17 +0100

    Removed directory sanitising code

--- Core/Parser.cs
+++ Core/Parser.cs
@@ -6,16 +6,6 @@ namespace EdGps.Core
 {
     public static class Parser
     {
-        public static string ParseDirectoryPath(string directoryPath) {
-            var charsToRemove = new string[] { "'", "&", "\"" };
-            var sanitizePath = directoryPath;
-
-            foreach (var c in charsToRemove)
-                sanitizePath = sanitizePath.Replace(c, string.Empty);
-
-            return sanitizePath.Trim(' ');
-        }
-
         public static Dictionary<string, object> ParseJson(string line) 
             => JsonConvert.DeserializeObject<Dictionary<string, object>>(line);
 
