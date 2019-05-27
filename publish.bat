041ecd64be9365924e5dbf093e82788f61bcd548
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 14:45:02 +0100

    Name change

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
-dotnet publish -o selfcontained-x86 -c Release -r win-x86 --self-contained
-dotnet publish -o selfcontained-x64 -c Release -r win-x64 --self-contained
+dotnet publish -o EdGps-x86 -c Release -r win-x86 --self-contained
+dotnet publish -o EdGps-x64 -c Release -r win-x64 --self-contained
