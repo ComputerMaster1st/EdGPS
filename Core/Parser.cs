a71e440080ad85f1d743dc1af0678fd5a2a2f41b
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 01:06:00 +0100

    Added directory path sanitizer

--- Core/Parser.cs
+++ Core/Parser.cs
@@ -2,9 +2,14 @@ namespace EdGps.Core
 {
     public static class Parser
     {
-        public static string ParseDirectoryPath(string directoryPath)
-            => directoryPath.Trim(new char[] { '\'', '&', '"' })
-                .TrimStart(' ')
-                .TrimEnd(' ');
+        public static string ParseDirectoryPath(string directoryPath) {
+            var charsToRemove = new string[] { "'", "&", "\"" };
+            var sanitizePath = directoryPath;
+
+            foreach (var c in charsToRemove)
+                sanitizePath = sanitizePath.Replace(c, string.Empty);
+
+            return sanitizePath.Trim(' ');
+        }
     }
 }
