de6ad66b931478125430fe2efb46669cd64be6d1
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 13:18:08 +0100

    Created Parent

---
+++ Core/Models/Parent.cs
@@ -0,0 +1,10 @@
+namespace EdGps.Core.Models
+{
+    public class Parent
+    {
+        public int Star { get; set; } = -1;
+        public int Planet { get; set; } = -1;
+        public int Ring { get; set; } = -1;
+        public int Null { get; set; } = -1;
+    }
+}
