699136069c85c0f6bc8c5cd30e5e52b8edaac109
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 14:21:09 +0100

    Added more event handlers

--- Gps.cs
+++ Gps.cs
@@ -21,26 +21,42 @@ namespace EdGps
         public async Task StartAsync() {
             _reader.OnAllBodiesFound += OnAllBodiesFoundAsync;
             _reader.OnBodyScan += OnBodyScanAsync;
+            _reader.OnDssScan += OnSurfaceScanAsync;
             _reader.OnFsdJump += OnEnteringNewSystemAsync;
+            _reader.OnFssDiscoveryScan += OnSystemHonkAsync;
 
             _system = await StarSystem.LoadAsync() ?? new StarSystem("Waiting...", new List<double>() { 0, 0, 0 });
             _reader.Start();
         }
 
-        private async void OnBodyScanAsync(object sender, Body body) {
-            _system.AddBody(body);
-            _writer.Write(_system);
-            await _system.SaveAsync();
-        }
-
-        private async void OnAllBodiesFoundAsync(object sender, bool isAllFound) {
-            _system.IsComplete = isAllFound;
-            _writer.Write(_system);
-            await _system.SaveAsync();
+        private async void OnSystemHonkAsync(object sender, FssDiscoveryScan scan) {
+            _system.TotalBodies = scan.BodyCount;
+            _system.TotalNonBodies = scan.NonBodyCount;
+            _system.IsHonked = true;
+            await WriteAndSaveAsync();
         }
 
         private async void OnEnteringNewSystemAsync(object sender, FsdJump system) {
             _system = new StarSystem(system.Name, system.Coordinates);
+            await WriteAndSaveAsync();
+        }
+
+        private async void OnSurfaceScanAsync(object sender, DssScan scan) {
+            _system.DssScanned(scan);
+            await WriteAndSaveAsync();
+        }
+
+        private async void OnBodyScanAsync(object sender, Body body) {
+            _system.AddBody(body);
+            await WriteAndSaveAsync();
+        }
+
+        private async void OnAllBodiesFoundAsync(object sender, bool isAllFound) {
+            _system.IsComplete = isAllFound;
+            await WriteAndSaveAsync();
+        }
+
+        private async Task WriteAndSaveAsync() {
             _writer.Write(_system);
             await _system.SaveAsync();
         }
