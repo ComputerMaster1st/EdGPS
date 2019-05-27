790960c9ccebef4bf572f8ef70a18e7f08f2a7d2
SpReeD <wackermann@nexn.systems>
2019-05-27 15:54:54 +0200

    Added some basic error handling in ED Log path, added reuse of previous legit path

--- Program.cs
+++ Program.cs
@@ -12,23 +12,95 @@ namespace EDCurrentSystem
         private ConsoleOutput conOutput = new ConsoleOutput();
 
         static void Main(string[] args) {
-            Console.Write("E:D Journal Log Directory :");
-            var rawDirectory = Console.ReadLine()
-                .Replace('\'', ' ')
-                .Replace('&', ' ')
-                .TrimStart(' ')
-                .TrimEnd(' ');
+            string rawDirectory = null;
+            string exittext = "\nPress any key to exit...";
 
-            if (rawDirectory.Length < 1) {
-                Console.WriteLine("Please specify the directory where E:D Journal logs are kept.");
+            //Check if user has already set a path
+            // NOTE: This should be changed to some common config file, like a config.INI or something for future settings
+            string edLogDirectoryFile = Path.Join(System.AppContext.BaseDirectory, "userdirectory.txt");
+            try
+            {
+                if(File.Exists(edLogDirectoryFile))
+                {
+                    rawDirectory = File.ReadAllText(edLogDirectoryFile);
+                }
+                else{
+                    Console.Write("E:D Journal Log Directory (e.g. C:\\Users\\<username>\\Saved Games\\Frontier Developments\\Elite Dangerous) :");
+                    rawDirectory = Console.ReadLine()
+                        .Replace('\'', ' ')
+                        .Replace('&', ' ')
+                        .TrimStart(' ')
+                        .TrimEnd(' ');
+
+                    if (rawDirectory.Length <= 4)
+                    {
+                        Console.WriteLine("Please specify the directory where E:D Journal logs are kept.");
+                        Console.WriteLine(exittext);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(exittext);
+                Console.ReadKey();
                 return;
             }
+            
+            //Some user error handling
+            DirectoryInfo directory = null;
+            FileInfo log = null;
+            try
+            {
+                if(!(Directory.Exists(rawDirectory)))
+                {
+                    throw new Exception("\nError: Directory does not exist.\n");
+                }
+                if(Directory.GetFiles(rawDirectory).Length <= 0)
+                {
+                    throw new Exception("\nError: Directory has no files.\n");
+                }
+                bool islogfile = false;
+                foreach (string file in Directory.GetFiles(rawDirectory))
+                {
+                    if (file.EndsWith(".log"))
+                    {
+                        islogfile = true;
+                    }
+                }
+                if(!islogfile)
+                    {
+                        throw new Exception("\nError: Directory has no *.log files.\n");
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(exittext);
+                Console.ReadKey();
+                Main(null);
+            }
 
-            var directory = new DirectoryInfo(rawDirectory);
-            var log = directory.GetFiles().Where(f => f.Extension == ".log")
+            //Save Path if legit
+            try
+            {
+                File.WriteAllText(edLogDirectoryFile, rawDirectory);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.WriteLine(exittext);
+                Console.ReadKey();
+                return;
+            }
+            
+            directory = new DirectoryInfo(rawDirectory);
+            log = directory.GetFiles().Where(f => f.Extension == ".log")
                 .OrderByDescending(f => f.LastWriteTime)
                 .First();
-            
+
             if (!Directory.Exists(Directories.SystemDir)) Directory.CreateDirectory(Directories.SystemDir);
 
             new Program().RunAsync(log.ToString()).GetAwaiter().GetResult();
