b159f6528a559222a27aadd168c2f560ca28689a
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 13:18:52 +0100

    Created Body. Will be used inside StarSystem

---
+++ Core/Models/Body.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace EdGps.Core.Models
+{
+    public class Body
+    {
+        public int Id { get; }
+        public string Name { get; set; } = string.Empty;
+        public BodyType Type { get; set; } = BodyType.Other;
+        public string SubType { get; set; } = string.Empty;
+        public List<int> Parents { get; set; } = new List<int>();
+        public double Distance { get; set; } = -1;
+        public bool Discovered { get; set; }
+        public bool Mapped { get; set; }
+        public string Terraformable { get; set; } = string.Empty;
+        public bool IsDssScanned { get; set; }
+
+        public List<Body> SubBodies { get; set; } = new List<Body>();
+
+        [JsonConstructor]
+        private Body(int id, string name, List<int> parents, double distance, string subType, bool discovered, bool mapped) {
+            Id = id;
+            Name = name;
+            SubType = subType;
+            Parents = parents;
+            Distance = distance;
+            Discovered = discovered;
+            Mapped = mapped;
+        }
+
+        public Body(ScanBody body) {
+            Id = body.Id;
+            Name = body.Name;
+            Parents = body.Parents;
+            Distance = body.Distance;
+            SubType = body.SubType;
+            Discovered = body.Discovered;
+            Mapped = body.Mapped;
+            Terraformable = body.Terraformable;
+        }
+        
+        public Body(int bodyId) => Id = bodyId;
+    }
+}
