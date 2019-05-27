d3a2996d723dbd6b21837154b400c9e659226526
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 13:19:14 +0100

    Added ScanBody

--- Core/Parser.cs
+++ Core/Parser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using EdGps.Core.Models;
 using Newtonsoft.Json;
 
 namespace EdGps.Core
@@ -20,5 +21,16 @@ namespace EdGps.Core
 
         public static T ParseJournalEvent<T>(Dictionary<string, object> rawData) where T : class, IJournalEvent
             => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(rawData));
+        
+        public static Body ParseScanBody(Dictionary<string, object> rawData) {
+            var isDiscovered = rawData.ContainsKey("WasDiscovered");
+            var isMapped = rawData.ContainsKey("WasMapped");
+            var body = new Body(JsonConvert.DeserializeObject<ScanBody>(JsonConvert.SerializeObject(rawData)));
+
+            if (!isDiscovered) body.Discovered = true;
+            if (!isMapped) body.Mapped = true;
+
+            return body;
+        }
     }
 }
