c29d335073bb29bebdc6bda0223274ccc11ada4d
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 01:33:31 +0100

    Added FileSystemWatcher

--- Core/JournalReader.cs
+++ Core/JournalReader.cs
@@ -1,16 +1,42 @@
+using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Elite_Dangerous_Galactic_Positioning_System.Core
 {
     public class JournalReader
     {
+        private CancellationTokenSource _cancelReader = null;
+        private FileSystemWatcher watcher = new FileSystemWatcher();
+
         public JournalReader(string journalDirectory) {
             var directory = new DirectoryInfo(journalDirectory);
-            var journal = directory.GetFiles()
+            var journalPath = directory.GetFiles()
                 .Where(f => f.Extension == ".log")
                 .OrderByDescending(f => f.LastWriteTime)
                 .First();
+
+            watcher.Path = journalDirectory;
+            watcher.NotifyFilter = NotifyFilters.LastAccess 
+                | NotifyFilters.LastWrite
+                | NotifyFilters.FileName;
+            watcher.Filter = "*.log";
+            watcher.Created += OnCreated;
+        }
+
+        public async Task StartAsync() {
+            _cancelReader?.Dispose();
+            _cancelReader = new CancellationTokenSource();
+        }
+
+        private async Task RunAsync(FileInfo journalFile, CancellationToken token) {
+            throw new NotImplementedException();
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e) {
+            throw new NotImplementedException();
         }
     }
 }
