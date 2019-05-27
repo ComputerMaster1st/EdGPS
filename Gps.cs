8b96dbf407d743a6492e29b93965c5fb915e4446
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-05-27 13:45:06 +0100

    Created JournalReader instance

--- Gps.cs
+++ Gps.cs
@@ -1,13 +1,18 @@
 using System.Threading.Tasks;
+using EdGps.Core;
 
 namespace EdGps
 {
     public class Gps
     {
-        public Gps(string directoryPath) {
+        private JournalReader _reader;
 
+        public Gps(string directoryPath) {
+            _reader = new JournalReader(directoryPath);
         }
 
-        
+        public void Start() {
+            _reader.Start();
+        }
     }
 }
