335e840d8b3c9d36c774ce51dd67d576603fe946
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-03 16:09:11 +0100

    Play sound

--- VoicePlayer.cs
+++ VoicePlayer.cs
@@ -1,11 +1,16 @@
 using EdGps.Core;
+using NAudio.Wave;
 
 namespace EdGps
 {
     public static class VoicePlayer
     {
         public static void Play(VoiceType response) {
-            
+            using (var reader = new Mp3FileReader($"{Directories.VoiceDir}/{response.ToString()}.mp3")) 
+            using (var waveOut = new WaveOutEvent()) {
+                waveOut.Init(reader);
+                waveOut.Play();
+            }
         }
     }
 }
