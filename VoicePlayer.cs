a6400106415ed83568dafdcca6555ac93c08f746
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-03 20:36:02 +0100

    Sound now working!

--- VoicePlayer.cs
+++ VoicePlayer.cs
@@ -1,11 +1,17 @@
+using System;
+using System.IO;
+using System.Windows.Media;
 using EdGps.Core;
 
 namespace EdGps
 {
     public static class VoicePlayer
     {
+        private static readonly MediaPlayer _wmp = new MediaPlayer();
+
         public static void Play(VoiceType response) {
-            
+            _wmp.Open(new Uri(Path.GetFullPath(Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3"))));
+            _wmp.Play();
         }
     }
 }
