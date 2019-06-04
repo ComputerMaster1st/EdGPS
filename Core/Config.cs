1c4d8f9332c9f6003d8825ab0cc28c38934ddb36
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-04 16:09:14 +0100

    Created config

---
+++ Core/Config.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using Newtonsoft.Json;
+
+namespace EdGps.Core
+{
+    [JsonObject(MemberSerialization.Fields)]
+    public class Config
+    {
+        [JsonIgnore]
+        private const string _configName = "config.json";
+
+        public string JournalPath { get; private set; } = string.Empty;
+        public bool VoiceEnabled { get; private set; } = false;
+
+        [JsonConstructor]
+        private Config(string journalPath, bool enableVoice) {
+            JournalPath = journalPath;
+            VoiceEnabled = enableVoice;
+        }
+
+        private Config() {}
+
+        public static Config LoadOrCreate() {
+            if (File.Exists(_configName)) return JsonConvert.DeserializeObject<Config>(File.ReadAllText(_configName));
+            return new Config();
+        }
+
+        private void Save()
+            => File.WriteAllText(_configName, JsonConvert.SerializeObject(this, Formatting.Indented));
+        
+        public void SetJournalPath(string path) {
+            JournalPath = path;
+            Save();
+        }
+
+        public void EnableVoice(bool enable = true) {
+            VoiceEnabled = enable;
+            Save();
+        }
+    }
+}
