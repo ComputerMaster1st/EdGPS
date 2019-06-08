21c0024ba01036457f68d5a1f0ebbe127d31d9e2
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-08 11:22:29 +0100

    Added NAudio to grab voice length to use for delay

--- VoicePlayer.cs
+++ VoicePlayer.cs
@@ -1,27 +1,34 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using EdGps.Core;
+using NAudio.Wave;
 
 namespace EdGps
 {
     public class VoicePlayer
     {
         private readonly SemaphoreSlim _lock = new SemaphoreSlim(1);
+        private readonly Dictionary<VoiceType, TimeSpan> _mp3Times = new Dictionary<VoiceType, TimeSpan>();
 
         public async Task Play(VoiceType response) {
             await _lock.WaitAsync();
 
+            var localFilePath = Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3");
+
+            if (!_mp3Times.ContainsKey(response)) {
+                using var mp3File = new Mp3FileReader(Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3"));
+                _mp3Times.Add(response, mp3File.TotalTime);
+            }
+
             var _wmp = new MediaPlayer();
-            _wmp.Open(new Uri(Path.GetFullPath(Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3"))));
-
-            while (!_wmp.NaturalDuration.HasTimeSpan) await Task.Delay(50);
-
+            _wmp.Open(new Uri(Path.GetFullPath(localFilePath)));
             _wmp.Play();
 
-            await Task.Delay(_wmp.NaturalDuration.TimeSpan);
+            await Task.Delay(_mp3Times[response]);
             _lock.Release();
         }
     }
