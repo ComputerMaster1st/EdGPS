168a89c1f140edcb80f97bc772501f72596076b9
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-10 22:11:25 +0100

    Get base star value

--- Core/BodyValue.cs
+++ Core/BodyValue.cs
@@ -1,10 +1,29 @@
-﻿namespace EdGps.Core
+﻿using System;
+
+namespace EdGps.Core
 {
     public static class BodyValue
     {
+        private const double StarFactor = 66.25;
+
         private const int Star = 1200;
         private const int NeutronStar = 22628;
         private const int BlackHole = 22628;
         private const int WhiteDwarf = 14057;
+
+        public static int GetStarValue(BodyType type, double mass) {
+            switch (type) {
+                case BodyType.Star:
+                    return (int)Math.Round(Star + ((mass * Star) / StarFactor));
+                case BodyType.Black_Hole:
+                    return (int)Math.Round(BlackHole + ((mass * BlackHole) / StarFactor));
+                case BodyType.Neutron_Star:
+                    return (int)Math.Round(NeutronStar + ((mass * NeutronStar) / StarFactor));
+                case BodyType.White_Dwarf:
+                    return (int)Math.Round(WhiteDwarf + ((mass * WhiteDwarf) / StarFactor));
+                default:
+                    return 0;
+            }
+        }
     }
 }
