b1b21f7f0c2baa3212c0575688a200aed445fe59
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2019-06-10 22:04:06 +0100

    Created BodyValue

---
+++ Core/BodyValue.cs
@@ -0,0 +1,10 @@
+﻿namespace EdGps.Core
+{
+    public static class BodyValue
+    {
+        private const int Star = 1200;
+        private const int NeutronStar = 22628;
+        private const int BlackHole = 22628;
+        private const int WhiteDwarf = 14057;
+    }
+}
