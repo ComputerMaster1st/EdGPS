875cee5ed01f0f0db324db27f34270189e99baf7
SpReeD <wackermann@nexn.systems>
2019-06-20 04:13:38 +0200

    VoicePlayer.cs using NAudio

--- VoicePlayer.cs
+++ VoicePlayer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Media;
 using EdGps.Core;
 using NAudio.Wave;
 
@@ -19,14 +18,24 @@ namespace EdGps
 
             var localFilePath = Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3");
 
-            if (!_mp3Times.ContainsKey(response)) {
-                using var mp3File = new Mp3FileReader(Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3"));
-                _mp3Times.Add(response, mp3File.TotalTime);
+            if (!_mp3Times.ContainsKey(response))
+            {
+                using (var mp3File = new Mp3FileReader(Path.Combine(Directories.VoiceDir, $"{response.ToString()}.mp3")))
+                {
+                   _mp3Times.Add(response, mp3File.TotalTime);
+                }
+                
             }
 
-            var _wmp = new MediaPlayer();
-            _wmp.Open(new Uri(Path.GetFullPath(localFilePath)));
-            _wmp.Play();
+            WaveStream mainOutputStream = new Mp3FileReader(Path.GetFullPath(localFilePath)); //Loading file to WaveStream
+            WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream); //Creating WaveChannel with default settings, set volume later
+            WaveOutEvent waveOut = new WaveOutEvent //NOTE: using WaveOutEvent and NOT WaveOut - WaveOut isn't compatible with console apps
+            {
+                DeviceNumber = -1, //Device -1 is the system default device
+                Volume = 1.0f //Volume could be set through config.json
+            };
+            waveOut.Init(volumeStream);
+            waveOut.Play();
 
             await Task.Delay(_mp3Times[response]);
             _lock.Release();
